e7d4fcbe3aef695b7bb8af506bb4de8932df3c4a
Patrick Tjin <pattjin@google.com>
2017-12-27 15:14:45 -0500

    pipa: Add AppWidget permission
    Change-Id: Iebcf1467936e37105bf0a6c71cb88caba33243d0

--- device.mk
+++ device.mk
@@ -36,6 +36,10 @@ DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
     $(LOCAL_PATH)/overlay-lineage
 
+# Permissions
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.software.app_widgets.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.app_widgets.xml
+
 # Shipping API level
 PRODUCT_SHIPPING_API_LEVEL := 30
 
