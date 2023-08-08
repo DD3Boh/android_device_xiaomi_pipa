af0afbcdc441f90bfb9f2a7b9afcfefe4487e79d
Fabian Leutenegger <fabian.leutenegger@bluewin.ch>
2023-08-08 22:33:47 +0200

    pipa: Add Xiaomi Peripheral Manager
     * Right now just an empty app that registers boot completed.
    
    Signed-off-by: Davide Garberi <dade.garberi@gmail.com>
    Change-Id: I7020b043e9e643d125e9641b18f3f0e71d8343b1

--- device.mk
+++ device.mk
@@ -36,6 +36,10 @@ DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
     $(LOCAL_PATH)/overlay-lineage
 
+# Peripheral Manager
+PRODUCT_PACKAGES += \
+    XiaomiPeripheralManager
+
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/handheld_core_hardware.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/handheld_core_hardware.xml \
