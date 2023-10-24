8faaa087b90530cd4e356dceec813d6e010fb15b
Davide Garberi <dade.garberi@gmail.com>
2023-10-24 14:21:58 +0200

    pipa: Add missing handheld core permissions
     * This gets automatically included when using the
       full_base_telephony makefile, while it doesn't
       with just full_base.
    
    Change-Id: Ic01d6fbef500fbc53d6c1e7351d08fe23d4880d5

--- device.mk
+++ device.mk
@@ -36,6 +36,10 @@ DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
     $(LOCAL_PATH)/overlay-lineage
 
+# Permissions
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/handheld_core_hardware.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/handheld_core_hardware.xml
+
 # Shipping API level
 PRODUCT_SHIPPING_API_LEVEL := 30
 
